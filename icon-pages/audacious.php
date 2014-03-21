ae45dfbd7ae5fcb7618da7bcb0a32097c6e113ac
Bruno Patyn <bp@netzkoop.de>
2014-03-21 22:24:32 +0100

    add smarty templates

---
+++ icon-pages/audacious.php
@@ -0,0 +1,15 @@
+<?php
+
+// put full path to Smarty.class.php
+require('/usr/local/lib/php/Smarty/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir('/var/www/smarty/templates');
+$smarty->setCompileDir('/var/www/smarty/templates_c');
+$smarty->setCacheDir('/var/www/smarty/cache');
+$smarty->setConfigDir('/var/www/smarty/configs');
+
+$smarty->assign('name', 'audacious');
+$smarty->display('skiva/icon-page.tpl');
+
+?>
